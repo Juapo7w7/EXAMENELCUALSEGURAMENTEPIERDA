eb6288c7b5b681fa0aa508b688b50bdfc33cd24c
Oliver MOlina <Olivermolina697@gmail.com>
2023-06-15 08:23:16 -0600

    Se ha agregado el Encabezado

---
+++ operaciones.py
@@ -0,0 +1,12 @@
+#Debe de realizar un algoritmo el cual resuelva los siguientes problemas de
+#matemática:
+# Suma (+)
+# Resta (-)
+# Multiplicación (*)
+# División (/)
+# Exponenciación (**)
+# Raíces (Función sqrt() de la librería math)
+# Suma de 5 números
+# Residuo (%)
+# Quetzal (Q7.91) a Dólar ($1)
+# Dólar ($1) a Quetzal (Q7.91)
