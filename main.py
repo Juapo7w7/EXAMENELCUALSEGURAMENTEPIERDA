b8652cd3ffdecd237743de0d231646601b4232d2
Oliver MOlina <Olivermolina697@gmail.com>
2023-06-15 11:42:27 -0600

    Se  a agregado acces (el cual no se usar) y se a agregado python

---
+++ main.py
@@ -0,0 +1,34 @@
+print ("Cual de las siguientes operaciones Desea ultilizar ")
+print ("1.Suma ")
+print ("2. Resta")
+print ("3. Multiplicacion ")
+print ("4. Division")
+print ("5. Exponenciacion")
+print ("6. Raiz Caudrada ")
+print ("7. Suma de Cinco de cinco digitos ")
+print ("8. Residuo ")
+print ("9. Pasar Quetzales a Dolares ")
+print ("10. Pasar Dolares a Quetzsales ")
+tipooperacion=(int(input("Ingrese el numero de operacion que desea realizar ")))
+if tipooperacion==1:
+    from operaciones import suma
+    a=(int(input("Ingrese un primer numero ")))
+    b=(int (input("Ingrese otro numero "))) 
+    totalsuma= suma(a,b)
+    print (totalsuma)
+
+elif tipooperacion==2:
+    from operaciones import resta
+    a=(int(input("Ingrese un primer numero ")))
+    b=(int(input("Ingrese el numero por lo cual quiere restarlo ")))
+    totalresta= resta(a,b)
+    print (totalresta)
+
+elif tipooperacion==3:
+    from operaciones import multiplicacion
+    a=(int(input("Ingrese un primer numero ")))
+    b=(int(input("Ingrese el numero por lo cual quiere restarlo ")))
+    totalmultiplicacion= multiplicacion(a,b)
+    print (totalmultiplicacion)
+
+
--- operaciones.py
+++ operaciones.py
@@ -10,3 +10,35 @@
 # Residuo (%)
 # Quetzal (Q7.91) a Dólar ($1)
 # Dólar ($1) a Quetzal (Q7.91)
+import math
+
+def suma (a,b):
+    resultado= a+b
+    return resultado
+
+def resta (a,b):
+    return a-b
+
+def multiplicacion (a,b):
+    return a*b
+
+def division (a,b):
+    return a/b 
+
+def exponenciacion (a,b):
+    return a**b
+
+def raizcuadrada (a):
+    return math.sqrt(a)
+
+def sumadecinco (a,b,c,d,e):
+    return a+b+c+d+e
+
+def residuo (a,b):
+    return a%b
+
+def QaD (a,b):
+    return a/b
+
+def DaQ (a,b):
+    return a*b
